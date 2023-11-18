742fd3e4b19d345d74f18acf230d3187f916dd85
Braden Goodwin <6876787+bradentg@users.noreply.github.com>
2023-11-18 18:12:36 -0500

    Add landing page image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
+import styles from './styles.module.css'
 import { Albert_Sans } from 'next/font/google'
 
 const albert_sans = Albert_Sans({
@@ -14,12 +16,14 @@ const Page = (): React.JSX.Element => {
       {/* <a href="bp2" className="max-w-sm rounded-md overflow-hidden shadow-xl transition hover:scale-102 hover:cursor-pointer my-2 sm:mx-2 bg-slate-100">
           <div className="font-bold text-4xl mb-2 bg-slate-100 w-full h-full">Beanpack 2 Official Server</div>
       </a> */}
-      <div id="main-page" className="grid h-screen w-screen">
-        <div id="server-button-container" className={albert_sans.className + ' h-screen w-1/3 content-center justify-self-center'}>
-          <Link href="bp2">
-          <div className="bg-blue-500 rounded-md shadow-xl transition hover:scale-102 py-4 my-2 text-white text-center">
-            <span className="font-bold text-2xl"><span className={albert_sans.className}>Beanpack 2 Official Server</span></span>
-          </div>
+      <div id="main-page" className="grid h-full w-full">
+        <Image priority={true} objectFit='cover' fill={true} className='opacity-70 absolute z-0 blur' src='/tubacz_house.png' alt='Minecraft House' />
+        <div id="server-button-container" className='absolute z-10 h-full w-1/3 content-center justify-self-center'>
+          <Image width='500' height='100' src="/beanmc_text.png" alt="BeanMC" />
+          <Link href="https://beanmc-wiki-6c65b75bf7ea.herokuapp.com/">
+            <div className='bg-blue-500 rounded-md shadow-xl transition hover:scale-102 py-4 my-2 text-white text-center'>
+              <span className="font-bold text-2xl"><span className={albert_sans.className}>Beanpack 2 Wiki</span></span>
+            </div>
           </Link>
           <div className="bg-blue-500 rounded-md shadow-xl transition py-4 my-2 text-white text-center opacity-50">
             <span className="font-bold text-2xl">Beanpack 3: Wizards & Wheels</span>
