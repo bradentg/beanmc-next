b627ac28126a0f72145e32114a0336e1af19d66b
Braden Goodwin <6876787+bradentg@users.noreply.github.com>
2023-11-18 18:15:06 -0500

    Fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import styles from './styles.module.css'
 import { Albert_Sans } from 'next/font/google'
 
-const albert_sans = Albert_Sans({
+const albertSans = Albert_Sans({
   weight: ['300'],
   subsets: ['latin']
 })
@@ -22,7 +21,7 @@ const Page = (): React.JSX.Element => {
           <Image width='500' height='100' src="/beanmc_text.png" alt="BeanMC" />
           <Link href="https://beanmc-wiki-6c65b75bf7ea.herokuapp.com/">
             <div className='bg-blue-500 rounded-md shadow-xl transition hover:scale-102 py-4 my-2 text-white text-center'>
-              <span className="font-bold text-2xl"><span className={albert_sans.className}>Beanpack 2 Wiki</span></span>
+              <span className="font-bold text-2xl"><span className={albertSans.className}>Beanpack 2 Wiki</span></span>
             </div>
           </Link>
           <div className="bg-blue-500 rounded-md shadow-xl transition py-4 my-2 text-white text-center opacity-50">
