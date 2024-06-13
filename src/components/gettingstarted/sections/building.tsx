332512efd508812a7babea22a366d2b687339c0b
Braden Goodwin <6876787+bradentg@users.noreply.github.com>
2024-06-13 17:15:43 -0400

    Test commit from kubuntu

--- src/components/gettingstarted/sections/building.tsx
+++ src/components/gettingstarted/sections/building.tsx
@@ -4,9 +4,6 @@ const Building = (): React.JSX.Element => {
   return (
     <div className="space-y-4">
         <p>
-          MineColonies builder speed is doubled!!!
-        </p>
-        <p>
           Create Schematicannon
         </p>
     </div>
