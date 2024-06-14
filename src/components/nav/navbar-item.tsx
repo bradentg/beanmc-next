e13fecb3134faf05b1ad90efa3e1a3643aec7e37
Braden Goodwin <6876787+bradentg@users.noreply.github.com>
2024-06-14 12:33:15 -0400

    Bluemap opens in new tab

--- src/components/nav/navbar-item.tsx
+++ src/components/nav/navbar-item.tsx
@@ -7,18 +7,29 @@ import { usePathname } from 'next/navigation'
 interface NavItemProps {
   text: string
   route: string
+  newTab?: boolean
 }
 
-const NavBarItem = ({ text, route }: NavItemProps): React.JSX.Element => {
+const NavBarItem = ({ text, route, newTab = false }: NavItemProps): React.JSX.Element => {
   const pathname = usePathname()
 
-  return (
-    <Link href={route}>
-      <div className={`inline mx-7 p-4 border border-none rounded-lg hover:text-purple-400 ${pathname === route ? 'outline outline-3 outline-offset-2 outline-purple-300' : ''}`}>
-        {text}
-      </div>
-    </Link>
-  )
+  if (newTab) {
+    return (
+      <a href={route} target="_blank">
+        <div className="inline mx-7 p-4 border border-none rounded-lg hover:text-purple-400">
+          {text}
+        </div>
+      </a>
+    )
+  } else {
+    return (
+      <Link href={route}>
+        <div className={`inline mx-7 p-4 border border-none rounded-lg hover:text-purple-400 ${pathname === route ? 'outline outline-3 outline-offset-2 outline-purple-300' : ''}`}>
+          {text}
+        </div>
+      </Link>
+    )
+  }
 }
 
 export default NavBarItem
